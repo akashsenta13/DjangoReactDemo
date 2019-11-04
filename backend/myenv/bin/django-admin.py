b8da125c34cf10f273376ccdffc5d40d4e0f95c3
AkashSenta <akashsenta@hotmail.com>
2019-11-04 17:47:20 +0530

    first commit updated

--- backend/myenv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/springpc-1/python/Projects/djreactdemo/backend/myenv/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
